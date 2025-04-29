7314f8213f43994fcb4c01659ec657735c795125
namddanh <namddanh@gmail.com>
2025-04-28 21:41:59 -0400

    added script and started implementing logic on buttons

---
+++ index.js
@@ -0,0 +1,6 @@
+const countAddBtn = document.getElementById("add");
+const countMinusBtn = document.getElementById("minus");
+
+countAddBtn.addEventListener("click", () => {
+  console.log("Hello");
+});
