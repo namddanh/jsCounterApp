aa662d8412396a1622e56d9eb782864c08b93816
namddanh <namddanh@gmail.com>
2025-04-28 21:48:30 -0400

    implemented the minus button

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+let count = 0;
+
 const countAddBtn = document.getElementById("add");
-const countMinusBtn = document.getElementById("minus");
+const countMinusBtn = document.getElementById("subtract");
+const countHeader = document.getElementById("count");
+
+countHeader.innerText = count;
 
 countAddBtn.addEventListener("click", () => {
-  console.log("Hello");
+  count++;
+  countHeader.innerHTML = count;
+});
+
+countMinusBtn.addEventListener("click", () => {
+  count--;
+  countHeader.innerHTML = count;
 });
